c587af1a560b28fe724a437eabdd176d35fbf82c
AlekseyKorobov <Okorobow@gmail.com>
2017-01-23 21:08:56 +0200

    save delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,6 +3,8 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+  private  int countResume=0;
+
 
     void clear() {
 
@@ -10,26 +12,32 @@ public class ArrayStorage {
 
     void save(Resume r) {
         Integer intUuid = Integer.parseInt(r.uuid);
-        storage [intUuid] = r ;
+        storage[intUuid] = new Resume();
+        storage[intUuid] = r;
+        ++countResume;
     }
 
     Resume get(String uuid) {
-        return null;
+        Integer intUuid = Integer.parseInt(uuid);
+        return storage[intUuid];
     }
 
     void delete(String uuid) {
         Integer intUuid = Integer.parseInt(uuid);
-        storage [intUuid] = null ;
+        storage[intUuid] = null;
+        --countResume;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        return storage ;
+                //new Resume[0];
     }
 
     int size() {
-        return 0;
+        return countResume;
     }
 }

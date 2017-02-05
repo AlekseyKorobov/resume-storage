bccfc9b92405999146a4de4708cb0ca1c8f5af26
AlekseyKorobov <Okorobow@gmail.com>
2017-02-05 21:31:40 +0200

    save delete get

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -11,23 +11,57 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        Integer intUuid = Integer.parseInt(r.uuid);
-        storage[intUuid] = new Resume();
-        storage[intUuid] = r;
-        ++countResume;
+
+        boolean notFindEquil = true ;
+        for (int i = 0; i <storage.length-1 ; i++) {
+            if (storage[i].uuid==r.uuid){ //проверка на уникальность
+                notFindEquil = false ;
+                System.out.println("Резюме с данным номером уже сущ.Перезаписать? Yes/No");
+                //здесь будет блок Input с консоли с последующей проверкой
+                if (yes){storage[i]=r;}
+                else { break;}
+            }
+
+        }
+        // запись резюме в пустой элент массива если оно новое
+        if (notFindEquil) {
+            for (int j = 0; j < storage.length - 1; j++) {
+                if (storage[j] == null) {
+                    storage[j] = new Resume();
+                    storage[j] = r;
+                    ++countResume;// счетчик резюме
+                    break;
+                }
+            }
+        }
     }
 
-    Resume get(String uuid) {
-        Integer intUuid = Integer.parseInt(uuid);
-        return storage[intUuid];
+    String get(String uuid) {
+        boolean notFindEquil = true ;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i].uuid = uuid) {
+                return storage[i];
+                notFindEquil = false;
+                break;
+            }
+        }
+       if(notFindEquil) return "резюме под данным номером отсутствует";
     }
 
     void delete(String uuid) {
-        Integer intUuid = Integer.parseInt(uuid);
-        storage[intUuid] = null;
-        --countResume;
-    }
 
+        for (int i = 0; i < countResume; i++)
+            if (storage[i].uuid = uuid) {
+                for (int j = i; j <= countResume - 1; j++) { //сдвигаем все резюме справо налево для отсутсвия null в середине
+                    storage[j] = storage[j + 1];
+                }
+                storage[countResume] = null;
+                --countResume;
+                break;
+            } else {
+                System.out.println("резюме под данным номером отсутствует");
+            }
+    }
     /**
      * @return array, contains only Resumes in storage (without null)
      */
